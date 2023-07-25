51bde673c5b382392fd00e5374b6bc7031790ac4
Thomas Bakken <tbakken14@gmail.com>
2023-07-25 10:44:12 -0700

    Add animal controller
    Co-authored-by: Pier Rodriguez <pierknows@gmail.com>
    Co-authored-by: Hernan Verar <Hernanverar@gmail.com>
    Co-authored-by: ESC18 <ponderosprintingus@gmail.com>

---
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using AnimalShelter.Models;
+
+public class AnimalController : Controller 
+{
+    [HttpGet("/Animals")]
+    public ActionResult Index()
+    {
+        List<Animal> animals = Animal.GetAll();
+        return View(new {animals = animals});
+    }
+
+    [HttpPost("/Animals/Delete")]
+    public ActionResult Delete(int id)
+    {
+        Animal.Clear(int id);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet("Animals/New")]
+    public ActionResult New()
+    {
+        return View();
+    }
+
+    [HttpPost("Animals/New")]
+    public ActionResult New(string name, string type, string description)
+    {
+        Animal.Create(name, type, desciption);
+        return RedirectToAction("Index", "Home");
+    }
+}
+
